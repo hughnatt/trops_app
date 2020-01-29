1884980ef435d763012c61f2977e01d7b1127e18
mathuld <mathieu.vincent1997@orange.fr>
2020-01-29 16:40:49 +0100

    Create ProfilePage Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'home.dart';
+import 'profile.dart';
 
 void main() => runApp(MyApp());
 
---
+++ lib/profile.dart
@@ -0,0 +1,5 @@
+class ProfilePage extends StatelessWidget {
+
+
+
+}
