54579e6e61aff9f2e2043f5955f1159b517f74eb
qsibue <quentin.sibue@live.fr>
2020-02-03 17:25:39 +0100

    can add picture from gallery & camera

--- lib/createAdvert.dart
+++ lib/createAdvert.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/rendering/box.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreateAdvertPage extends StatefulWidget {
 
@@ -9,6 +12,49 @@ class CreateAdvertPage extends StatefulWidget {
 
 class _CreateAdvertPage extends State<CreateAdvertPage> {
 
+  List<File> imageFile = [null,null,null,null];
+
+  _openGallery(BuildContext context, int index) async{
+    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState((){
+      this.imageFile[index] = picture;
+    });
+  }
+
+  _openCamera(BuildContext context,int index) async{
+    var picture = await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState((){
+      this.imageFile[index] = picture;
+    });
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context, int index){
+    return showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text("How to import picture ?"),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              GestureDetector(
+                child: Text("Load picture"),
+                onTap: (){
+                  _openGallery(context,index);
+                },
+              ),
+
+              Padding(padding: EdgeInsets.all(8.0)),
+              GestureDetector(
+                child: Text("Take picture"),
+                onTap: (){
+                  _openCamera(context,index);
+                },
+              )
+            ],
+          ),
+        ),
+      );
+    });
+  }
 
   Widget _buildMultilineTextField(int nbLines, String label) {
     return TextField(
@@ -29,24 +75,38 @@ class _CreateAdvertPage extends State<CreateAdvertPage> {
       crossAxisCount: 2,
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
-      children: List.generate(3, (index) {
+      children: List.generate(4, (index) {
         return Center(
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1.0),
+          child: GestureDetector(
+            onTap: (){
+              _showChoiceDialog(context,index);
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 3.0),
+              ),
+              width: 500,
+              height: 500,
+              child: _boxContent(index),
             ),
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Picture ' + (index+1).toString(),
-              style: Theme.of(context).textTheme.headline,
-            ),
-          ),
+          )
         );
       }),
     );
   }
 
+Widget _boxContent(int index){
+    if(imageFile[index] == null){
+      return Text(
+        'Picture ' + (index+1).toString(),
+        style: Theme.of(context).textTheme.headline,
+      );
+    }
+    else{
+      return Image.file(imageFile[index],fit: BoxFit.cover);
+    }
 
+}
 
   @override
   Widget build(BuildContext context) {
@@ -76,7 +136,6 @@ class _CreateAdvertPage extends State<CreateAdvertPage> {
                   ),
                   child: _buildPictureGrild(),
                 ),
-
               ],
             )
             )
