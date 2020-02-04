9daebd7b51028356a478f3fe55e998b94574cfff
Hugh Natt <hughnatt@protonmail.com>
2020-02-04 10:27:54 +0100

    Add login/register page

---
+++ lib/utils/bubble_indication_painter.dart
@@ -0,0 +1,75 @@
+/*
+MIT License
+
+Copyright (c) 2018 Hugo EXTRAT
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+Original Version :  https://github.com/huextrat/TheGorgeousLogin/
+ */
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class TabIndicationPainter extends CustomPainter {
+  Paint painter;
+  final double dxTarget;
+  final double dxEntry;
+  final double radius;
+  final double dy;
+
+  final PageController pageController;
+
+  TabIndicationPainter(
+      {this.dxTarget = 125.0,
+        this.dxEntry = 25.0,
+        this.radius = 21.0,
+        this.dy = 25.0, this.pageController}) : super(repaint: pageController) {
+    painter = new Paint()
+      ..color = Color(0xFFFFFFFF)
+      ..style = PaintingStyle.fill;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final pos = pageController.position;
+    double fullExtent = (pos.maxScrollExtent - pos.minScrollExtent + pos.viewportDimension);
+
+    double pageOffset = pos.extentBefore / fullExtent;
+
+    bool left2right = dxEntry < dxTarget;
+    Offset entry = new Offset(left2right ? dxEntry: dxTarget, dy);
+    Offset target = new Offset(left2right ? dxTarget : dxEntry, dy);
+
+    Path path = new Path();
+    path.addArc(
+        new Rect.fromCircle(center: entry, radius: radius), 0.5 * pi, 1 * pi);
+    path.addRect(
+        new Rect.fromLTRB(entry.dx, dy - radius, target.dx, dy + radius));
+    path.addArc(
+        new Rect.fromCircle(center: target, radius: radius), 1.5 * pi, 1 * pi);
+
+    canvas.translate(size.width * pageOffset, 0.0);
+    canvas.drawShadow(path, Color(0x552B2B2B), 3.0, true);
+    canvas.drawPath(path, painter);
+  }
+
+  @override
+  bool shouldRepaint(TabIndicationPainter oldDelegate) => true;
+}
