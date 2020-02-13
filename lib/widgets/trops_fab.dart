aa19ab59d2a2f015c7cee5849e28dc280acbc9c9
Ariane Ancrenaz <ancrenaz.ariane@hotmail.fr>
2020-02-13 08:59:55 +0100

    now can't create advert if not logged in

--- lib/widgets/trops_fab.dart
+++ lib/widgets/trops_fab.dart
@@ -2,13 +2,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:trops_app/ui/createAdvert.dart';
+import 'package:trops_app/models/User.dart';
 
 class TropsFloatingActionButton extends StatelessWidget {
+
+  void _onButtonPressed(BuildContext context){
+    if(User.current != null){
+      Navigator.push(context, MaterialPageRoute(builder: (context) => CreateAdvertPage()));
+    } else {
+      Navigator.pushNamed(context, "/auth");
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => CreateAdvertPage()));}
+        onPressed: () => _onButtonPressed(context),
         );
   }
 }
