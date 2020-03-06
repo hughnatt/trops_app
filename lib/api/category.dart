c40cc0e74aeb7db2b9dbb17d38aaf4180f235b39
Hugh Natt <hughnatt@protonmail.com>
2020-03-06 12:05:13 +0100

    Set defaults for category thumbnail and descriptions

--- lib/api/category.dart
+++ lib/api/category.dart
@@ -16,9 +16,14 @@ Future<List<TropsCategory>> getCategories() async {
     var result = await jsonDecode(response.body);
 
 
-
     result.forEach((item) {
-      TropsCategory tropsCategory = TropsCategory(item['_id'], item['name'], "", "", getSubcategories(item['name'],item['children']));
+      if (item['description'] == null){
+        item['description'] = "";
+      }
+      if (item['thumbnail'] == null){
+        item['thumbnail'] = "";
+      }
+      TropsCategory tropsCategory = TropsCategory(item['_id'], item['name'], item['description'], item['thumbnail'], getSubcategories(item['name'],item['children']));
       categories.add(tropsCategory);
       _categoryNameCache.addAll({item['_id'] : item['name']});
     });
@@ -35,8 +40,14 @@ List<TropsCategory> getSubcategories(parentName,json) {
   if (json != null) {
     json.forEach((item) {
       var name = parentName + "/" + item['name'];
+      if (item['description'] == null){
+        item['description'] = "";
+      }
+      if (item['thumbnail'] == null){
+        item['thumbnail'] = "";
+      }
       TropsCategory tropsCategory = TropsCategory(
-          item['_id'], item['name'], "", "", getSubcategories(name,item['children']));
+          item['_id'], item['name'], item['description'], item['thumbnail'], getSubcategories(name,item['children']));
       subcategories.add(tropsCategory);
       _categoryNameCache.addAll({item['_id'] : name});
     });
