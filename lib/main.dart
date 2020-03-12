bf7abdbe9463f5fde03b250d23ce96423bbd4d54
Hugh Natt <hughnatt@protonmail.com>
2020-03-12 09:04:19 +0100

    Rename searchresult to search

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:trops_app/ui/auth.dart';
 import 'package:trops_app/ui/createAdvert.dart';
 import 'package:trops_app/ui/profile.dart';
-import 'package:trops_app/ui/searchresult.dart';
+import 'package:trops_app/ui/search.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:trops_app/ui/splash.dart';
 import 'ui/home.dart';
